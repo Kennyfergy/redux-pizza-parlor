6121b613e23558f43c7003d452968c367fe1e062
T Mark <tmk1.tran@gmail.com>
2023-09-28 13:55:15 -0500

    npm install

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
+import { useDispatch } from 'react-redux';
+
 
 function App() {
-
+  const dispatch = useDispatch();
+  useEffect(() => {getPizzas()},
+  []);
+  // GET axios w/ function
+const getPizzas = () => {
+  axios.get("/api/pizza")
+  .then((response) => {
+    dispatch({ type: "SET_PIZZAS", payload: response.data})
+  })
+  .catch((error) => {
+    console.log("Error in getting pizza list", error)
+  });
+};
+  // POST axios w/ function
+const postPizzas = () {
+  axios.post("/api/pizza")
+  .then((response) => {
+    dispat
+  })
+}
+  
   return (
     <div className='App'>
       <header className='App-header'>
